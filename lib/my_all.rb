847ba5377893c0b5d2a911bd1c972c9cfc488e4c
J Fern <jfern20@gmail.com>
2018-08-13 04:40:09 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -6,9 +6,14 @@ def my_all?(collection)
  while i < collection.length 
   block_return_values << yield(collection[i])
   i += 1 
+ end
  
-end
+ if block_return_values.include?(false)
+   false
+else 
+  true 
 
+ end 
 end 
 
 
